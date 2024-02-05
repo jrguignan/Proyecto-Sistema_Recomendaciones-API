9ebd51f59fa6ad68546bff41544182f157f65957
Jose R. Guignan <jrguignan@users.noreply.github.com>
2024-02-05 07:52:41 -0500

    update

--- funciones_api.py
+++ funciones_api.py
@@ -50,9 +50,9 @@ def presentacion():
         <body>
             <h1>API de consultas de videojuegos de la plataforma Steam</h1>
             <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
-            <p>INSTRUCCIONES:</p>
+            <p>Instrucciones:</p>
             <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
-            <p> El desarrollo de este proyecto esta en <a href="https://github.com/jrguignan/Proyecto-Sistema_Recomendaciones-API"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
+            <p> El siguiente repositorio contiene el código del proyecto <a href="https://github.com/jrguignan/Proyecto-Sistema_Recomendaciones-API"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
         </body>
     </html>
     '''
